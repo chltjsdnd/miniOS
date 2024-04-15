726f0c8227d936ce98caf3d18783853ca4738378
chltjsdnd <163288780+chltjsdnd@users.noreply.github.com>
2024-04-15 14:10:40 +0900

    Add files via upload

--- include/system.h
+++ include/system.h
@@ -3,3 +3,9 @@
 //SSU struct task_struct {
 
 void minisystem();
+int custom_fork();
+void ipc_pipe();
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
